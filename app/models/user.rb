db138d177c6fe79880dc01d879b433895548a6b9
vasylluk <vasyl3luk@gmail.com>
2017-12-16 19:36:29 +0200

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, :password, :email, presence: true
+
+ devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+end
